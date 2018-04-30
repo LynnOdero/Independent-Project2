5fdb1cdee82355d91c0300fd2b7470766c829304
LynnOdero <oderolynn@gmail.com>
2018-04-30 15:15:00 +0300

    more changes

--- Project-Two/js/restuarant.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-$(".btn-subscribe").on("click",function () {
-	console.log("Clicked");
-     var emailAddress = $("#Email").val();
-     alert("**" + emailAddress +" has been successfully added to our email list.Thank you. **") 
-});
-});
